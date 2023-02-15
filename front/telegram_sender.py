6662e0ea3defbf83ec2cf051d71044231af02cb1
Kirill <k_vasuirin@mail.ru>
2023-02-15 21:24:35 +0300

    Набросан интерфей сфронта проекта в первом приближении

---
+++ front/telegram_sender.py
@@ -0,0 +1,27 @@
+"""
+Файл с функциями отправки сообщений по каналам. Скорее всего в итоге он будет иметь другой вид.
+"""
+
+
+def send_to_sber_less_then_one(data_to_be_sent):
+    pass
+
+
+def send_to_tink_less_then_one(data_to_be_sent):
+    pass
+
+
+def send_to_vip(data_to_be_sent):
+    pass
+
+
+def send_to_qiwi(data_to_be_sent):
+    pass
+
+
+def send_to_binance_garantex(data_to_be_sent):
+    pass
+
+
+def send_to_no_bank(data_to_be_sent):
+    pass
