ff970968ba457e2d39a9bef196521d016ff50fab
bushanka <61285033+bushanka@users.noreply.github.com>
2023-02-16 17:11:17 +0300

    Добавлена логика бота

--- front/configs.py
+++ front/configs.py
@@ -1,3 +1,12 @@
 """
 Файл с конфигами для фронта. Тут будут лежать ссылки на чаты, токены для ботов, и прочее.
 """
+
+BOT_TOKEN = '5730376351:AAE78gpQmHLkZ1PGyHpaTO5qCNBfkSOfgz8'
+SBER_LESS_THAN_ONE_CHANNEL_ID = ''
+TINK_LESS_THAN_ONE_CHANNEL_ID = ''
+VIP_CHANNEL_ID = ''
+QIWI_CHANNEL_ID = ''
+BINANCE_GARANTEX_CHANNEL_ID = ''
+NO_BANK_CHANNEL_ID = ''
+TEST_CHANNEL_ID = -1001461773186
